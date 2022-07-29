10efd5e16ef71ad2191088164ef5173b61527df1
clianor <ejrgus94160@gmail.com>
2022-07-29 23:27:10 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return <div tw="text-xl text-blue-500 ring m-8">Home</div>;
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const colors = require('tailwindcss/colors');
+
+delete colors['lightBlue'];
+delete colors['warmGray'];
+delete colors['trueGray'];
+delete colors['coolGray'];
+delete colors['blueGray'];
+
+module.exports = {
+  theme: {
+    colors,
+    extend: {},
+  },
+  plugins: [],
+};
